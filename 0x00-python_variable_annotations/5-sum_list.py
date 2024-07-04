29c3981cdebc7a5d1e86394f782cbd35c2247b6b
MohamedAmhal <mohammedamhal349@gmail.com>
2024-07-04 22:44:40 +0100

    addition:

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'''
+this is the function that sun all ites in a liste
+'''
+
+
+liste = list[float]
+
+
+def sum_list(input_list: liste) -> float:
+    '''
+    parameters:
+    @input_list : the list value
+    '''
+
+    return sum(input_list)
