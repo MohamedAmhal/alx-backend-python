30fe3d628b6918479dab4f31a5d6eaf1592e2ec8
MohamedAmhal <mohammedamhal349@gmail.com>
2024-07-04 22:04:27 +0100

    updating:

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''
+this function transforme a float to  a string
+'''
+
+
+def to_str(n: float) -> str:
+    '''
+    parametrers:
+    @n : float
+    '''
+
+    return str(n)
