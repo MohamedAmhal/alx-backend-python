0d247427af4c5320feb1b07ef59d069c4788ae40
MohamedAmhal <mohammedamhal349@gmail.com>
2024-07-04 23:55:55 +0100

    updating

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''
+documentation !!!
+'''
+
+
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    '''
+    docummentation !!!
+    '''
+
+    return (k, v ** 2)
