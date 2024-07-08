546d82256752728dafe5ba2eb0a65f9aff01016a
MohamedAmhal <mohammedamhal349@gmail.com>
2024-07-08 19:44:17 +0100

    updaatinng

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -10,7 +10,7 @@ import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     '''
     Parameters:
     max_delay (int): The maximum number of seconds to wait.
