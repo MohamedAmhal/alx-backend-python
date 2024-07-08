ebfd9f4f5c6d24be91ce84cf831bd0ab576cc3dd
MohamedAmhal <mohammedamhal349@gmail.com>
2024-07-08 19:42:20 +0100

    updating

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+'''
+This module contains a function that waits for a random delay between 0,
+and returns the duration of the delay.
+'''
+
+
+import asyncio
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    '''
+    Parameters:
+    max_delay (int): The maximum number of seconds to wait.
+    n (int): the number of looops
+
+    Returns:
+    list: The actual duration of the delay.
+    '''
+    code_start = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    return (time.time() - code_start) / n
